f4fc08eba00bb2079282c17f294b296a05a5e94b
hekanth <hekanth5597@gmail.com>
2023-04-27 00:11:53 +0530

    changes made in config,entity,util

--- src/main/java/com/jwt/security/controller/UserController.java
+++ src/main/java/com/jwt/security/controller/UserController.java
@@ -1,14 +1,41 @@
 package com.jwt.security.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.jwt.security.entity.AuthRequest;
+import com.jwt.security.util.JwtUtil;
+
+@RestController
+@RequestMapping("/")
 public class UserController {
-	
-	@GetMapping("/")
+
+	@GetMapping("/hi")
 	public String getUser() {
 		return "Welcome to springSecurity";
+	}
+
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@PostMapping("/authenticate")
+	public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+		} catch (Exception ex) {
+			throw new Exception("inavalid username/password");
+		}
+		return jwtUtil.generateToken(authRequest.getUserName());
 	}
 
 }
